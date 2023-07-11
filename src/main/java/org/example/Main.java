4436ffdda93d27faa8ca8924c3e9a79bc22b7e50
Knotty123230 <122396662+Knotty123230@users.noreply.github.com>
2023-07-11 22:55:25 +0300

    jdbc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:54321/postgres",
-                "myname", "mysecretpassword").load();
-        flyway.migrate();
+        extracted();
         List<Client> clients = new ClientService().listAll();
         System.out.println(clients);
 
+    }
+
+    private static void extracted() {
+        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:54321/postgres",
+                "myname", "mysecretpassword").load();
+        flyway.migrate();
     }
 }
